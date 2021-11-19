123f3c0be8f0669766ca998c941716135d5f6aec
IdoTav <92517147+IdoTav@users.noreply.github.com>
2021-11-19 00:01:03 -0800

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,20 @@
+import random
 import socket
+import string
 import sys
+import os
 
-if len(sys.argv) != 2:
-    exit()
-port = sys.argv[1]
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('', 12345))
-server.listen(5)
+server.listen(0)
+if len(sys.argv) != 2:
+    exit()
 while True:
     client_socket, client_address = server.accept()
     print('Connection from: ', client_address)
     data = client_socket.recv(100)
+    if data.decode('utf-8') == 0:
+        client_socket.send(''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(128)))
     print('Received: ', data)
     client_socket.send(data.upper())
     client_socket.close()
