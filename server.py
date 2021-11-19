8d6ec2871df48a3a2220679d4ebcdf78b0bb9c57
IdoTav <92517147+IdoTav@users.noreply.github.com>
2021-11-19 00:13:21 -0800

    Update server.py

--- server.py
+++ server.py
@@ -14,7 +14,8 @@ while True:
     print('Connection from: ', client_address)
     data = client_socket.recv(100)
     if data.decode('utf-8') == 0:
-        client_socket.send(''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(128)))
+        id_user = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(128))
+        client_socket.send(str.encode(id_user))
     print('Received: ', data)
     client_socket.send(data.upper())
     client_socket.close()
