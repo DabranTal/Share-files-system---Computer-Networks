b03dc99f52170d357448839084b105c0942842f1
IdoTav <92517147+IdoTav@users.noreply.github.com>
2021-11-19 03:14:45 -0800

    Update server.py

--- server.py
+++ server.py
@@ -4,6 +4,30 @@ import string
 import sys
 import os
 
+
+def data_analysis(data):
+    str_data = str(data)
+    header_data = []
+    for index in range(len(str_data) - 4, len(str_data), 4):
+        header_data.append(str_data[index: index + 4])
+    for index in range(len(str_data) - 5, len(str_data) - 4, 1):
+        header_data.append(str_data[index: index + 1])
+    for index in range(len(str_data) - 8, len(str_data) - 5, 3):
+        header_data.append(str_data[index: index + 3])
+    for index in range(len(str_data) - 40, len(str_data) - 8, 32):
+        header_data.append(str_data[index: index + 32])
+    n = int(header_data[3], 2)
+    for index in range(len(str_data) - 40 - n, len(str_data) - 40, n):
+        header_data.append(str_data[index: index + n])
+    for index in range(0, len(str_data) - 40 - n, len(str_data) - 40 - n):
+        header_data.append(str_data[index: index + len(str_data) - 40 - n])
+    return header_data
+
+
+def create_a_file(userid):
+    file = open(userid, "x")
+
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('', 12345))
 server.listen(0)
@@ -16,9 +40,10 @@ while True:
     if user_id.decode('utf-8') == 0:
         id_user = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(128))
         client_socket.send(str.encode(id_user))
-    else
-        clinet_socket.send(user_id)   
-    print('Received: ', data)
-    client_socket.send(data.upper())
+        create_a_file(id_user)
+    else:
+        client_socket.send(user_id)
+    print('Received: ', user_id)
+    client_socket.send(user_id.upper())
     client_socket.close()
     print('Client disconnected')
