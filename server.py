1e0a85af05ca778f924243710e72380a8adbd797
IdoTav <92517147+IdoTav@users.noreply.github.com>
2021-11-21 12:59:00 -0800

    Update server.py

--- server.py
+++ server.py
@@ -24,6 +24,52 @@ def upload_files_to_folder(file_name, folder_path, got_from_recv):
         client_socket.send(b'ack')
 
 
+def split_operations(operations):
+    operation_split = operations.split("|")
+    operation_list = []
+    for i in range(len(operation_split)):
+        if "" == operation_split[i]:
+            continue
+        operation_list.append(operation_split[i].split(","))
+    return operation_list
+
+
+def find_keys(data_dic, id):
+    key_list = []
+    for key in data_dic:
+        key_list.append(key)
+    for j in range(len(key_list)):
+        if key_list[j] == id:
+            return True
+    return False
+
+
+def data_structure(data_dic, id, port, header):
+    header_data = utils.data_analysis_2(header)
+    if id in data_dic:
+        if port in data_dic[id]:
+            to_do = data_dic[id][port]
+            data_dic[id][port] = ''
+            for i in data_dic[id]:
+                if i == port:
+                    continue
+                data_dic[id][i] = data_dic[id][i] + '|' + header_data[3] + ',' + header_data[0]
+        else:
+            to_do = data_dic[id][0]
+            for i in data_dic[id]:
+                if i == port:
+                    continue
+                data_dic[id][i] = data_dic[id][i] + '|' + header_data[3] + ',' + header_data[0]
+            data_dic[id][port] = ''
+    else:
+        data_dic[id] = {port: header_data[3] + ',' + header_data[0]}
+        data_dic[id][0] = header_data[3] + ',' + header_data[0]
+        to_do = header_data[3] + ',' + header_data[0]
+        data_dic[id][port] = ''
+    return data_dic, to_do
+
+
+data_dic = {}
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 server.bind(('', int(sys.argv[1])))
@@ -41,6 +87,7 @@ while True:
         # Create main user folder
         user_folder_path = create_a_folder(id_user, os.getcwd())
         header = client_socket.recv(1024)
+        data_dic, to_do = data_structure(data_dic, id_user, int(sys.argv[1]), header)
         while header != b'enough':
             header = utils.data_analysis_2(header)
             if ADD_FOLDER == int(header[0]):
@@ -57,6 +104,8 @@ while True:
                     new_file.close()
                     client_socket.send(b'ack')
             header = client_socket.recv(1024)
+            if header != b'enough':
+                data_dic, to_do = data_structure(data_dic, id_user, int(sys.argv[1]), header)
     else:
         client_socket.send(user_id)
     client_socket.close()
