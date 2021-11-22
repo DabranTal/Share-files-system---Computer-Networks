e9af8e02d33c8ec4d5820581d95ea00791dd39b4
Tal Dabran <dabrantal@gmail.com>
2021-11-22 15:52:30 +0200

    #begin watchdog

--- client.py
+++ client.py
@@ -16,44 +16,22 @@ SEGMENT_SIZE = 1024
 INT_IN_BITS = 32
 BITS_ON_BYTE = 8
 
-"""""
+
 def on_created(event):
     print(f"hey, {event.src_path} has been created!")
-    utils.update_folders(main_folder, event.src_path, server_socket, user_id, CREATE)
+
 
 
 def on_deleted(event):
     print(f"what the f**k! Someone deleted {event.src_path}!")
-    utils.update_folders(main_folder, event.src_path, server_socket, user_id, DELETE)
+
 
 
 def on_modified(event):
     print(f"hey buddy, {event.src_path} has been modified")
-    utils.update_folders(main_folder, event.src_path, server_socket, user_id, UPDATE)
 
-patterns = ["*"]
-ignore_patterns = None
-ignore_directories = False
-case_sensitive = True
-my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
 
-my_event_handler.on_created = on_created
-my_event_handler.on_deleted = on_deleted
-my_event_handler.on_modified = on_modified
 
-path = "."
-go_recursively = True
-my_observer = Observer()
-my_observer.schedule(my_event_handler, main_folder.path, go_recursively)
-
-my_observer.start()
-try:
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    my_observer.stop()
-    my_observer.join()
-"""""
 # Initialize all the variable we got as arguments
 ip_server = sys.argv[1]
 port_server = sys.argv[2]
@@ -67,6 +45,25 @@ if len(sys.argv) == 6:
 backslash = utils.get_backslash()
 my_directory = os.listdir(folder_path)
 main_folder = utils.Folder(folder_path)
+patterns = ["*"]
+ignore_patterns = None
+ignore_directories = False
+case_sensitive = True
+my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
+my_event_handler.on_created = on_created
+my_event_handler.on_deleted = on_deleted
+my_event_handler.on_modified = on_modified
+path = "."
+go_recursively = True
+my_observer = Observer()
+my_observer.schedule(my_event_handler, main_folder.path, go_recursively)
+my_observer.start()
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    my_observer.stop()
+    my_observer.join()
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 server_socket.connect((ip_server, int(port_server)))
